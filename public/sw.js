2ece9a29b28b222a759d944fe80d17f881bc956f
pansilupabasara20050101-svg <pansilupabasara20050101@gmail.com>
2026-06-02 04:57:56 +0530

    Fix PWA caching

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'hazel-v1';
-const ASSETS = ['/', '/index.html'];
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
